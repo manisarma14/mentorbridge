d43e3ba5f28cc6f033ab9ca8ea829608ce43baef
MANI SARMA <manisarma@MANIs-MacBook-Air.local>
2026-03-24 13:13:59 +0530

    fixed cors for production

--- server/app.js
+++ server/app.js
@@ -12,10 +12,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://mentorbridge.vercel.app"
-  ],
+  origin: true,
   credentials: true,
 }));
 
